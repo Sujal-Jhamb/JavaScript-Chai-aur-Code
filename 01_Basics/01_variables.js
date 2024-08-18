5790199950840ddc8ce8ef23e020fb0bc632ec96
Sujal Jhamb <157912241+Sujal-Jhamb@users.noreply.github.com>
2024-08-18 16:45:32 +0000

    dataTypes covered

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553;
+let accountEmail = "js@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2 // Not allowed
+
+accountEmail = "hehe@gmail.com";
+accountPassword = "2121";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+console.log(accountEmail);
+
+// Shortcut
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var
+because of issue in block scope and function scope
+*/
---
+++ 02_DataTypes/02_dataTypes.js
@@ -0,0 +1,27 @@
+"usue strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodeJs, not browser
+
+console.log(3 + 3)
+
+console.log("Sujal")
+
+let name = "Sujal"
+let age = 18
+let isLoggedIN = false
+// primitive data types
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => stand alone value
+undefined
+symbol => unique
+*/
+
+// object
+console.log(typeof age)
+console.log(typeof "Sujal")
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
