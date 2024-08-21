ecbbec069f4ea1e91ec92fdba7c9a2ccc9374d16
Sujal Jhamb <157912241+Sujal-Jhamb@users.noreply.github.com>
2024-08-21 07:21:28 +0000

    Conversion of dataTypes

---
+++ 02_DataTypes/03_dataTypesOperation.js
@@ -0,0 +1,43 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score = "33abc"
+// let numericalValue = Number(score)
+// console.log(numericalValue)
+
+// let score = null
+// console.log(typeof score)
+// let value = Number (score)
+// console.log(value);
+
+// let score = undefined
+// console.log(typeof score)
+// let value = Number (score)
+// console.log(value);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sujal" => true
+
+let someNumber = 33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
